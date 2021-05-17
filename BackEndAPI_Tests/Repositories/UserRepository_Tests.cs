2fa8b6ce86829dea060fce96e41679a8cfc8b27c
Doc1997 <anhtranduc1997@gmail.com>
2021-05-17 18:29:16 +0700

    feat/3: Added unit tests

---
+++ BackEndAPI_Tests/Repositories/UserRepository_Tests.cs
@@ -0,0 +1,176 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackEndAPI.DBContext;
+using BackEndAPI.Entities;
+using BackEndAPI.Enums;
+using BackEndAPI.Interfaces;
+using BackEndAPI.Repositories;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+
+namespace BackEndAPI_Tests.Repositories
+{
+    [TestFixture]
+    public class UserRepository_Tests
+    {
+        private AssetsManagementDBContext _context;
+        private IAsyncUserRepository _repository;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task Create_AddNewUser_WritesToDatabase()
+        {
+
+            //Arrange
+            User user = new User()
+            {
+                Id = 3,
+                StaffCode = "SD0003",
+                FirstName = "Thang",
+                LastName = "Doan Viet",
+                DateOfBirth = new DateTime(1995, 06, 03),
+                JoinedDate = new DateTime(2021, 12, 05),
+                Gender = Gender.Male,
+                Type = UserType.Admin,
+                UserName = "thangdv",
+                Password = "thangdv@30601995",
+                Location = Location.HaNoi,
+                Status = UserStatus.Active
+            };
+            var options = new DbContextOptionsBuilder<AssetsManagementDBContext>()
+                    .UseInMemoryDatabase(databaseName: "Create_AddNewUser_WritesToDatabase")
+                .Options;
+
+            _context = new AssetsManagementDBContext(options);
+            _repository = new UserRepository(_context);
+
+            //Act
+            var _user = await _repository.Create(user);
+            await _context.SaveChangesAsync();
+
+            //Assert
+            Assert.AreEqual(1, _context.Users.Count());
+
+            Assert.AreEqual("SD0003", _context.Users.Single().StaffCode);
+
+            Assert.AreEqual("Thang", _context.Users.Single().FirstName);
+
+            Assert.AreEqual("Doan Viet", _context.Users.Single().LastName);
+
+            Assert.AreEqual(new DateTime(1995, 06, 03), _context.Users.Single().DateOfBirth);
+
+            Assert.AreEqual(new DateTime(2021, 12, 05), _context.Users.Single().JoinedDate);
+
+            Assert.AreEqual(Gender.Male, _context.Users.Single().Gender);
+
+            Assert.AreEqual(UserType.Admin, _context.Users.Single().Type);
+
+            Assert.AreEqual("thangdv", _context.Users.Single().UserName);
+
+            Assert.AreEqual("thangdv@30601995", _context.Users.Single().Password);
+
+            Assert.AreEqual(UserStatus.Active, _context.Users.Single().Status);
+
+            Assert.AreEqual(Location.HaNoi, _context.Users.Single().Location);
+
+        }
+
+        [TestCase(null)]
+        public void CountUsername_NullUsernameInserted_ThrowExceptionMessage(string username)
+        {
+
+            var options = new DbContextOptionsBuilder<AssetsManagementDBContext>()
+                        .UseInMemoryDatabase(databaseName: "CountUsername_NullUsernameInserted_ThrowExceptionMessage")
+                        .Options;
+            _repository = new UserRepository(_context);
+
+            var result = Assert.Throws<ArgumentNullException>(() => _repository.CountUsername(username));
+
+            Assert.AreEqual("Value cannot be null. (Parameter 'Username can not be null!')", result.Message);
+            //I don't know how to fix this one
+
+        }
+
+        [TestCase("thangdv")]
+        public void CountUsername_ValidUsernameInserted_ReturnNumberOfGivenUsername(string username)
+        {
+
+            var options = new DbContextOptionsBuilder<AssetsManagementDBContext>()
+                        .UseInMemoryDatabase(databaseName: "CountUsername_ValidUsernameInserted_ReturnNumberOfGivenUsername")
+                        .Options;
+
+            _context = new AssetsManagementDBContext(options);
+            _repository = new UserRepository(_context);
+
+            Assert.AreEqual(0, _repository.CountUsername(username));
+
+        }
+
+        [Test]
+        public async void GetAllUsers_Default_ShouldGetAllAvailableUsers()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<AssetsManagementDBContext>()
+                        .UseInMemoryDatabase(databaseName: "GetAllUsers_Default_ShouldGetAllAvailableUsers")
+                        .Options;
+
+            _context = new AssetsManagementDBContext(options);
+            _repository = new UserRepository(_context);
+
+            await _context.AddRangeAsync(
+                new User
+                {
+                    Id = 1,
+                    StaffCode = "SD0001",
+                    FirstName = "Binh",
+                    LastName = "Nguyen Van",
+                    DateOfBirth = new DateTime(1993, 01, 20),
+                    JoinedDate = new DateTime(2021, 12, 05),
+                    Gender = Gender.Male,
+                    Type = UserType.Admin,
+                    UserName = "binhnv",
+                    Password = "binhnv@20011993",
+                    Location = Location.HaNoi,
+                    Status = UserStatus.Active
+                },
+                new User
+                {
+                    Id = 2,
+                    StaffCode = "SD0002",
+                    FirstName = "Binh",
+                    LastName = "Nguyen Thi",
+                    DateOfBirth = new DateTime(1994, 01, 12).Date,
+                    JoinedDate = new DateTime(2021, 12, 05).Date,
+                    Gender = Gender.Female,
+                    Type = UserType.User,
+                    UserName = "binhnt",
+                    Password = "binhnt@12011994",
+                    Location = Location.HaNoi,
+                    Status = UserStatus.Active
+                }
+            );
+            await _context.SaveChangesAsync();
+
+            //Act
+            var users = _repository.GetAll();
+
+            //Assert
+            Assert.AreEqual(2, users.Count());
+        }
+
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _context.DisposeAsync();
+        }
+    }
+}
---
+++ BackEndAPI_Tests/Services_Tests/UserServices_Tests.cs
@@ -0,0 +1,301 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using BackEndAPI.Entities;
+using BackEndAPI.Enums;
+using BackEndAPI.Helpers;
+using BackEndAPI.Interfaces;
+using BackEndAPI.Models;
+using BackEndAPI.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace BackEndAPI_Tests.Services_Tests
+{
+    [TestFixture]
+    public class UserServices_Tests
+    {
+        private static IQueryable<User> Users
+        {
+            get
+            {
+                return new List<User>
+                {
+                    new User
+                    {
+                        Id = 1,
+                        StaffCode = "SD0001",
+                        FirstName = "Binh",
+                        LastName = "Nguyen Van",
+                        DateOfBirth = new DateTime(1993, 01, 20),
+                        JoinedDate = new DateTime(2021, 12, 05),
+                        Gender = Gender.Male,
+                        Type = UserType.Admin,
+                        UserName = "binhnv",
+                        Password = "binhnv@20011993",
+                        Location = Location.HaNoi,
+                        Status = UserStatus.Active
+                    },
+                    new User
+                    {
+                        Id = 2,
+                        StaffCode = "SD0002",
+                        FirstName = "Binh",
+                        LastName = "Nguyen Thi",
+                        DateOfBirth = new DateTime(1994, 01, 12).Date,
+                        JoinedDate = new DateTime(2021, 12, 05).Date,
+                        Gender = Gender.Female,
+                        Type = UserType.User,
+                        UserName = "binhnt",
+                        Password = "binhnt@12011994",
+                        Location = Location.HaNoi,
+                        Status = UserStatus.Active
+                    },
+                    new User{ Id = 3},
+                    new User{ Id = 4},
+                    new User{ Id = 5},
+                    new User{ Id = 6},
+                    new User{ Id = 7},
+                    new User{ Id = 8},
+                    new User{ Id = 9},
+                    new User{ Id = 10},
+                    new User{ Id = 11},
+                    new User{ Id = 12},
+                    new User{ Id = 13},
+                    new User{ Id = 14},
+                    new User{ Id = 15},
+                    new User{ Id = 16},
+                    new User{ Id = 17},
+                    new User{ Id = 18},
+                    new User{ Id = 19},
+                    new User{ Id = 20},
+                    new User{ Id = 21},
+                    new User{ Id = 22},
+                    new User{ Id = 23},
+                    new User{ Id = 24},
+                    new User{ Id = 25},
+                    new User{ Id = 26},
+                    new User{ Id = 27},
+                    new User{ Id = 28},
+                    new User{ Id = 29},
+                    new User{ Id = 30}
+                }
+                .AsQueryable();
+            }
+        }
+
+        private static IQueryable<Assignment> Assignments
+        {
+            get
+            {
+                return new List<Assignment>
+                {
+                    new Assignment
+                    {
+                        Id = 1,
+                        AssetId = 1,
+                        AssignedByUserId = 1,
+                        AssignedToUserId = 2,
+                        Note = "Testing1",
+                        State = AssignmentState.WaitingForAcceptance
+                    }
+                }
+                .AsQueryable();
+            }
+        }
+
+        private static IMapper _mapper;
+
+        private Mock<IAsyncUserRepository> _userRepositoryMock;
+        private Mock<IAsyncAssignmentRepository> _assignmentRepositoryMock;
+
+        public UserServices_Tests()
+        {
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new AutoMapperProfile());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _userRepositoryMock = new Mock<IAsyncUserRepository>(behavior: MockBehavior.Strict);
+            _assignmentRepositoryMock = new Mock<IAsyncAssignmentRepository>(behavior: MockBehavior.Strict);
+        }
+
+        [Test]
+        public void GetUsers_WithDefaultValidPaginationParameters_ShouldReturnProperlyPagedListResponse()
+        {
+            //Arrange
+            _userRepositoryMock.Setup(x => x.GetAll()).Returns(Users);
+            _assignmentRepositoryMock.Setup(x => x.CountUser(It.IsAny<int>())).Returns(1);
+            var userService = new UserService(
+                _userRepositoryMock.Object,
+                _assignmentRepositoryMock.Object,
+                _mapper
+            );
+
+            PaginationParameters parameters = new PaginationParameters
+            {
+                PageNumber = 1,
+                PageSize = 30
+            };
+
+            //Act
+            var usersPagedListResponse = userService.GetUsers(parameters);
+
+            //Assert
+            Assert.AreEqual(30, usersPagedListResponse.TotalCount);
+            Assert.AreEqual(1, usersPagedListResponse.CurrentPage);
+            Assert.AreEqual(1, usersPagedListResponse.TotalPages);
+            Assert.IsFalse(usersPagedListResponse.HasNext);
+            Assert.IsFalse(usersPagedListResponse.HasPrevious);
+        }
+
+        [TestCase(10)]
+        [TestCase(15)]
+        [TestCase(20)]
+        public void GetUsers_WithDifferentValidPageSize_ShouldReturnProperlyPagedListResponse(int pageSize)
+        {
+            //Arrange
+            _userRepositoryMock.Setup(x => x.GetAll()).Returns(Users);
+            _assignmentRepositoryMock.Setup(x => x.CountUser(It.IsAny<int>())).Returns(1);
+            var userService = new UserService(
+                _userRepositoryMock.Object,
+                _assignmentRepositoryMock.Object,
+                _mapper
+            );
+
+            var expectedTotalPages = (int)Math.Ceiling(Users.Count() / (double)pageSize);
+            List<PaginationParameters> parametersList = new List<PaginationParameters>();
+            for (int i = 1; i <= expectedTotalPages; i++)
+            {
+                parametersList.Add(
+                    new PaginationParameters
+                    {
+                        PageNumber = i,
+                        PageSize = pageSize
+                    }
+                );
+            }
+            
+            int actualCount = 0;
+            foreach (var parameters in parametersList)
+            {
+                //Act
+                var usersPagedListResponse = userService.GetUsers(parameters);
+                actualCount += usersPagedListResponse.Items.Count();
+
+                //Assert
+                Assert.AreEqual(parameters.PageNumber, usersPagedListResponse.CurrentPage);
+                Assert.AreEqual(expectedTotalPages, usersPagedListResponse.TotalPages);
+                Assert.AreEqual(parameters.PageNumber < expectedTotalPages, usersPagedListResponse.HasNext);
+                Assert.AreEqual(parameters.PageNumber > 1, usersPagedListResponse.HasPrevious);
+            }
+
+            Assert.AreEqual(Users.Count(), actualCount);
+
+        }
+
+        [Test]
+        public void GetUsers_WithNegativePageNumber_ShouldReturnPagedListResponseWithDefaultPageNumberOf1()
+        {
+            //Arrange
+            _userRepositoryMock.Setup(x => x.GetAll()).Returns(Users);
+            _assignmentRepositoryMock.Setup(x => x.CountUser(It.IsAny<int>())).Returns(1);
+            var userService = new UserService(
+                _userRepositoryMock.Object,
+                _assignmentRepositoryMock.Object,
+                _mapper
+            );
+
+            PaginationParameters parameters = new PaginationParameters
+            {
+                PageNumber = -1,
+                PageSize = 30
+            };
+
+            //Act
+            var usersPagedListResponse = userService.GetUsers(parameters);
+
+            //Assert
+            Assert.AreEqual(30, usersPagedListResponse.TotalCount);
+            Assert.AreEqual(1, usersPagedListResponse.CurrentPage);
+            Assert.AreEqual(1, usersPagedListResponse.TotalPages);
+            Assert.IsFalse(usersPagedListResponse.HasNext);
+            Assert.IsFalse(usersPagedListResponse.HasPrevious);
+        }
+
+        [Test]
+        public void GetUsers_WithPageSizeSmallerThanMinOf10_ShouldReturnPagedListResponseWithDefaultMinPageSizeOf10()
+        {
+            //Arrange
+            _userRepositoryMock.Setup(x => x.GetAll()).Returns(Users);
+            _assignmentRepositoryMock.Setup(x => x.CountUser(It.IsAny<int>())).Returns(1);
+            var userService = new UserService(
+                _userRepositoryMock.Object,
+                _assignmentRepositoryMock.Object,
+                _mapper
+            );
+
+            PaginationParameters parameters = new PaginationParameters
+            {
+                PageNumber = 1,
+                PageSize = 0
+            };
+
+            //Act
+            var usersPagedListResponse = userService.GetUsers(parameters);
+
+            //Assert
+            Assert.AreEqual(30, usersPagedListResponse.TotalCount);
+            Assert.AreEqual(1, usersPagedListResponse.CurrentPage);
+            Assert.AreEqual(3, usersPagedListResponse.TotalPages);
+            Assert.IsTrue(usersPagedListResponse.HasNext);
+            Assert.IsFalse(usersPagedListResponse.HasPrevious);
+        }
+
+        [Test]
+        public void GetUsers_WithPageSizeBiggerThanMaxOf50_ShouldReturnPagedListResponseWithDefaultMinPageSizeOf50()
+        {
+            //Arrange
+            _userRepositoryMock.Setup(x => x.GetAll()).Returns(Users);
+            _assignmentRepositoryMock.Setup(x => x.CountUser(It.IsAny<int>())).Returns(1);
+            var userService = new UserService(
+                _userRepositoryMock.Object,
+                _assignmentRepositoryMock.Object,
+                _mapper
+            );
+
+            PaginationParameters parameters = new PaginationParameters
+            {
+                PageNumber = 1,
+                PageSize = 100
+            };
+
+            //Act
+            var usersPagedListResponse = userService.GetUsers(parameters);
+
+            //Assert
+            Assert.AreEqual(30, usersPagedListResponse.TotalCount);
+            Assert.AreEqual(1, usersPagedListResponse.CurrentPage);
+            Assert.AreEqual(1, usersPagedListResponse.TotalPages);
+            Assert.IsFalse(usersPagedListResponse.HasNext);
+            Assert.IsFalse(usersPagedListResponse.HasPrevious);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+    }
+}
