4598823601c27b5529cd54334500a2fd2c6d3cbf
Anh Nguyen <huuanh171@gmail.com>
2021-06-03 10:36:59 +0700

    feat/26: Fix Bug

--- FrontEnd/src/services/report.service.tsx
+++ FrontEnd/src/services/report.service.tsx
@@ -1,5 +1,11 @@
 import { authHeader } from "../helpers/auth-header";
 import { handleResponse } from "../helpers/handle-response";
+import { authenticationService } from "./authentication.service";
+
+export enum Location {
+  HaNoi = 0,
+  HoChiMinh = 1,
+}
 
 export const reportService = {
   getReport,
@@ -8,15 +14,36 @@ export const reportService = {
 
 async function getReport() {
   const requestOptions = { method: "GET", headers: authHeader() };
-  const response = await fetch(`https://localhost:5001/api/reports`, requestOptions);
+  var response;
+  if (authenticationService.currentUserValue.location == Location.HaNoi) {
+    response = await fetch(
+      `https://localhost:5001/api/reports/report/HaNoi`,
+      requestOptions
+    );
+  }
+  if (authenticationService.currentUserValue.location == Location.HoChiMinh) {
+    response = await fetch(
+      `https://localhost:5001/api/reports/report/HoChiMinh`,
+      requestOptions
+    );
+  }
   return handleResponse(response);
 }
 
 async function exportExcel() {
   const requestOptions = { method: "GET", headers: authHeader() };
-  const response = await fetch(
-    `https://localhost:5001/api/Reports/ExportXls`,
-    requestOptions
-  );
+  var response;
+  if (authenticationService.currentUserValue.location == Location.HaNoi) {
+    response = await fetch(
+      `https://localhost:5001/api/Reports/ExportXls/HaNoi`,
+      requestOptions
+    );
+  }
+  if (authenticationService.currentUserValue.location == Location.HoChiMinh) {
+    response = await fetch(
+      `https://localhost:5001/api/Reports/ExportXls/HoChiMinh`,
+      requestOptions
+    );
+  }
   return handleResponse(response);
 }
